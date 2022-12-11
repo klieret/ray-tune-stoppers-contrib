b50c35711d93dee262295e4e260d0fc3c6299b5e
Kilian Lieret <kilian.lieret@posteo.de>
2022-12-11 13:00:27 -0500

    ✅ Expand tests of Threshold testing as well

--- test/test_threshold_by_epoch.py
+++ test/test_threshold_by_epoch.py
@@ -1,10 +1,26 @@
 from __future__ import annotations
 
+from copy import deepcopy
 from test import StopperTester
 
 import pytest
 
 from rt_stoppers_contrib.threshold_by_epoch import ThresholdByEpochStopper
+
+
+def get_reversed_experiment(st: StopperTester):
+    """Changes signs of metric results and sets mode to min instead of max
+    (and vice-versa)
+    """
+    stopper = deepcopy(st._stopper)
+    stopper._thresholds = {k: -v for k, v in stopper._thresholds.items()}
+    stopper._comparison_mode = "min" if stopper._comparison_mode == "max" else "max"
+    return StopperTester(
+        stopper=stopper,
+        metric_results=[-m for m in st._metric_results],
+        doesnt_stop=st._doesnt_stop,
+    )
+
 
 _cases = [
     StopperTester(
@@ -21,6 +37,7 @@ _cases = [
     ),
 ]
 cases = [c for c in _cases]
+cases.extend([get_reversed_experiment(c) for c in _cases])
 
 
 @pytest.mark.parametrize("case", cases)
