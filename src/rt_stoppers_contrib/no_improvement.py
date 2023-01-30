602ac269c15e1fcce03159a1eb79cabd9bd3d220
Kilian Lieret <kilian.lieret@posteo.de>
2023-01-30 15:40:46 -0500

    ✨ Allow dictionaries for patience and rel changes

--- src/rt_stoppers_contrib/no_improvement.py
+++ src/rt_stoppers_contrib/no_improvement.py
@@ -5,15 +5,17 @@ from typing import Any, DefaultDict
 
 from ray import tune
 
+from rt_stoppers_contrib.util.misc import _get_quantity_for_epoch
+
 
 class NoImprovementTrialStopper(tune.Stopper):
     def __init__(
         self,
         metric: str,
         *,
-        rel_change_thld: float = 0.01,
+        rel_change_thld: float | dict[int, float] = 0.01,
         mode: str = "max",
-        patience: int = 6,
+        patience: int | dict[int, int] = 6,
         grace_period: int = 4,
     ):
         """Stopper that stops trial if at no iteration within ``num_results`` a better
@@ -28,17 +30,24 @@ class NoImprovementTrialStopper(tune.Stopper):
             rel_change_thld: Relative change threshold to be considered for improvement.
                 Any change that is less than that is considered no improvement. If set
                 to 0, any change is considered an improvement.
-                Can also be set to a dictionary epoch -> threshold.
+                Can also be set to a dictionary epoch -> threshold (the first epoch
+                has the index 0).
             mode: "max" or "min"
             patience: Number of iterations without improvement after which to stop.
                 If 1, stop after the first iteration without improvement.
-                Can also be set to a dictionary epoch -> patience.
+                Can also be set to a dictionary epoch -> patience (the first epoch
+                has the index 0).
             grace_period: Number of iterations to wait before considering stopping
         """
         self._metric = metric
         self._rel_change_thld = rel_change_thld
         self._comparison_mode = mode
-        if patience < 1:
+
+        if isinstance(patience, dict):
+            min_patience = min(patience.values())
+        else:
+            min_patience = patience
+        if min_patience < 1:
             raise ValueError("Patience must be at least 1.")
         self._patience = patience
         self._grace_period = grace_period
@@ -48,11 +57,7 @@ class NoImprovementTrialStopper(tune.Stopper):
         self._stagnant: DefaultDict[Any, int] = collections.defaultdict(int)
         self._epoch: DefaultDict[Any, int] = collections.defaultdict(int)
 
-    @property
-    def rel_change_thld(self) -> float:
-        return self._rel_change_thld
-
-    def _better_than(self, a: float, b: float) -> bool:
+    def _better_than(self, a: float, b: float, epoch: int) -> bool:
         """Is result a better than result b?"""
         try:
             ratio = a / b
@@ -60,27 +65,29 @@ class NoImprovementTrialStopper(tune.Stopper):
             ratio = None
         if ratio is None:
             return False
-        if self._comparison_mode == "max" and ratio > 1 + self._rel_change_thld:
+        rc_thld = _get_quantity_for_epoch(self._rel_change_thld, epoch)
+        if self._comparison_mode == "max" and ratio > 1 + rc_thld:
             return True
-        if self._comparison_mode == "min" and ratio < 1 - self._rel_change_thld:
+        if self._comparison_mode == "min" and ratio < 1 - rc_thld:
             return True
         return False
 
     def __call__(self, trial_id: Any, result: dict[str, Any]) -> bool:
         self._epoch[trial_id] += 1
+        epoch = self._epoch[trial_id]
         if self._best[trial_id] is None:
             self._best[trial_id] = result[self._metric]
             return False
         best_result = self._best[trial_id]
         assert best_result is not None  # for mypy
-        if self._better_than(result[self._metric], best_result):
+        if self._better_than(result[self._metric], best_result, epoch):
             self._best[trial_id] = result[self._metric]
             self._stagnant[trial_id] = 0
             return False
         self._stagnant[trial_id] += 1
         if self._epoch[trial_id] < self._grace_period:
             return False
-        if self._stagnant[trial_id] >= self._patience:
+        if self._stagnant[trial_id] >= _get_quantity_for_epoch(self._patience, epoch):
             return True
         return False
 
